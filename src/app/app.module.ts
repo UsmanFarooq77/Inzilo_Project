6dc75df69d838bcaf8b42bb0d6bce58ee015199c
UsmanFarooq77 <usmanfarooq7721@gmail.com>
2021-07-29 16:59:06 +0500

    Implement the full responsiveness and delete the unused login component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
